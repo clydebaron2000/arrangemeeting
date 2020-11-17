df5a89fd940f73bbf9f82f19f984aafe5cdabe67
rancid <robmgraham13@yahoo.com>
2020-11-17 14:02:20 -0800

    added nav bar and jumbotron

---
+++ create-react-express/client/src/componets/Jumbotron/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron(){
+    return (
+        <div className = "jumbotron text-center">
+            <h1> Rendezvous</h1>
+        </div>
+    );
+}
+export default Jumbotron;
---
+++ create-react-express/client/src/componets/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css"
+
+function Nav() {
+    return (
+    <nav className= "navbar">
+        <a className="navbar-brand" href= "/">
+            Rendezvous
+            </a>
+    </nav>
+    );
+}
+
+export default Nav
