800a918902111ec47b8bf465a548c9aec3ce0f0c
unknown <pbsanders1@gmail.com>
2020-11-17 16:23:39 -0800

    added password hashing to security so passwords are not saved as plain text in the db

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
@@ -11,6 +12,26 @@ const userSchema = new mongoose.Schema({
     default: ""
   }
 });
+
+userSchema.methods = {
+  checkPassword: function (inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.password)
+  },
+  hashPassword: plainTextPassword => {
+    return bcrypt.hashSync(plainTextPassword, 10)
+  }
+}
+
+userSchema.pre('save', function (next) {
+  if (!this.password) {
+    console.log('models/user.js =======NO PASSWORD PROVIDED=======');
+    next();
+  } else {
+    console.log('models/user.js hashPassword in pre save');
+    this.password = this.hashPassword(this.password);
+    next();
+  }
+})
 
 const User = mongoose.model("User", userSchema);
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const Router = require('./routes/apiroutes.js');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +27,17 @@ connection.once('open', () => {
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
+
+//sessions
+app.use(session(
+    {  
+        secret: 'waggle-fraggle', //pick a random string to make the hash that is generated secure  resave: false, //required  saveUninitialized: false //required}))
+        resave: false, //required
+        saveUninitialized: false //required
+    })
+)
+
+app.use( (req, res, next) => {  console.log('req.session', req.session);  next()});
   
 
 
