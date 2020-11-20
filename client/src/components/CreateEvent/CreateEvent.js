3a6e39254f68b01e717766201a0c39b1c2c56835
rancid <robmgraham13@yahoo.com>
2020-11-19 20:09:27 -0800

    added modal component and modal into createEvent

--- client/src/components/CreateEvent/CreateEvent.js
+++ client/src/components/CreateEvent/CreateEvent.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./style.css"
+import Example from '../ModalWindow/modalWindow'
+
+
+
+
 
 const CreateEvent = () => (
-	<div>
-	  <h2>Create Event Page!</h2>
-	  <p><Link to="/">Head back to home</Link></p>
+	<div className= "createEventDiv">
+		
+	  <h2 className="createH2">Create Event Page!</h2>
+    <Example/>
+       
+	  <p><Link to="/">Head back to home</Link></p>	
 	</div>
 );
 
 export default CreateEvent;
+
---
+++ client/src/components/ModalWindow/modalWindow.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import ModalDialog from 'react-bootstrap/ModalDialog';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+import ModalFooter from 'react-bootstrap/ModalFooter';
+import Button from 'react-bootstrap/Button';
+
+
+function Modal1() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="success" onClick={handleShow}>
+        Launch static backdrop modal
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Create</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+
+
+export default Modal1;
+
