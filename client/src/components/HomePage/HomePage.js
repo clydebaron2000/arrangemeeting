ab5447d9fec92f25cc524b55e904bcfdbbf40b58
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-11-26 22:55:06 -0800

    changed className for <a> links to avoid universal styling and other conflicts

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
+import Navbar from "../Navbar/Navbar";
+
 
 export default class HomePage extends Component {
 
@@ -12,9 +14,10 @@ export default class HomePage extends Component {
         return (
 
 			<div>
+				<Navbar/>
 	  			<h2>Welcome to Rendezvous!</h2>
-				<p><Link to="/logIn">Login</Link></p>
-				<p><Link to="/signup">Sign Up</Link></p>
+				<p><Link to="/logIn" className="LoginButton">Login</Link></p>
+				<p><Link to="/signup" className="SignupButton">Sign Up</Link></p>
 	  		</div>
 		)
 		}
