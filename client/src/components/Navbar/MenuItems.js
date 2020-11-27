694e84cd26f4eb312a158b4a7a9570d52666481a
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-11-26 22:47:14 -0800

    created navbar component

---
+++ client/src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '/',
+        cName: 'nav-links'
+    },    
+    {
+        title: 'Create Event',
+        url: '#',
+        cName: 'nav-links'
+    },    
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links'
+    },    
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
