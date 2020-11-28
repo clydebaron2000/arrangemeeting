f97bff0ddd4bc1d267acbfe58379be83c2763dbe
rancid <robmgraham13@yahoo.com>
2020-11-28 11:11:15 -0800

    basic calander added, still needs adjustment to fit our liking but its up and running on view page

--- client/src/components/ViewEvent/ViewEvent.js
+++ client/src/components/ViewEvent/ViewEvent.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
+import DemoApp from "../Calender/calender"
 
 const ViewEvent = () => (
 	<div className= "viewEventDiv">
 		
 	  <h2 className="viewH2">view Event Page!</h2>
-    
+    <DemoApp/>
        
 	  <p><Link to="/">Head back to home</Link></p>	
 	</div>
