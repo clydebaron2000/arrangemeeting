f67248d066839af3e0f73cfbecc84b30894c90f4
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-12-03 20:51:23 -0800

    implemented data checks for incomming API data

--- client/src/components/CalendarGrid/CalendarGrid.jsx
+++ client/src/components/CalendarGrid/CalendarGrid.jsx
@@ -2,9 +2,31 @@ import React, {useEffect,useState} from 'react'
 import Calendar from '../Calender';
 import NamesList from '../NamesList';
 const CalendarGrid=(props)=>{
-    
-    
-    
-    return ()
+    //these won't change
+    const valid_dates=props.valid_dates;
+    const valid_times=props.valid_times;
+    //these will
+    const [calendar_matrix,setCalendar_matrix]=useState(props.calendar_matrix)
+    const [names_list,setNames_list]=useState(props.names_list)
+    useEffect(()=>{
+        const componentOutput={
+            calendar_matrix:calendar_matrix,
+            names_list:names_list
+        }
+        props.handleCalendarChange(componentOutput)
+    },[calendar_matrix,names_list]);//if these update, submit.
+    return (
+        <div>
+            <Calendar
+            valid_dates={valid_dates}
+            valid_times={valid_times}
+            calendar_num_matrix={}//
+            filter_by_name={}//filter calendar based on name
+            />
+            <NamesList
+            handleNamePress={}//hands the name string up that was pressed. only one name can be submitted at a time
+            />
+        </div>
+    )
 }
 export default CalendarGrid
--- client/src/pages/Event/event.jsx
+++ client/src/pages/Event/event.jsx
@@ -14,7 +14,37 @@ const EventPage = () => {
 		if (res.status==400){//if not found
 			return (<Redirect to='/'/>) // go to homepage
 		}
-		setData(res.data);//otherwise, set data
+		//checks of the data
+		const raw_data=res.data;
+		if (raw_data.url_end!=urlending){
+			return (
+				<div>Err: data recieved did not match data fetched`</div>
+			)
+		}
+		if (raw_data.name==""||raw_data.name==undefined){
+			return (
+				<div>Err: Event title empty</div>
+			)
+		}
+		const start_time=raw_data.valid_times.start;//number from 0 to 23.5
+		const end_time=raw_data.valid_times.end;//number from 0 to 23.5
+		const num_time_blocks=(end_time-start_time)/.5;
+		if (raw_data.calendar_matrix.length==0){
+			return (
+				<div>Err: calendar_matrix empty</div>
+			)
+		}
+		if (raw_data.calendar_matrix[0].length!=num_time_blocks){
+			return (
+				<div>Err: calendar_matrix does not have appropiate length</div>
+			)
+		}
+		if (typeof(raw_data.valid_dates)!=typeof(new Date())){
+			return (
+			<div>Err: valid_dates does not have appropiate type: {typeof(raw_data.valid_dates)}</div>
+			)
+		}
+		setData(raw_data);//otherwise, set data
 	})}
 	const updateData=_=>{
 		API.postEvent(eventData);
@@ -28,7 +58,7 @@ const EventPage = () => {
 		const new_data=temp_data;
 		setData(new_data);
 	}
-	
+
 	useEffect(()=>{
 		updateData(eventData)
 		fetchDataBy_urlending(urlending)
@@ -45,7 +75,7 @@ const EventPage = () => {
 		valid_times={eventData.valid_times}
 		calendar_matrix={eventData.calendar_matrix}
 		names_list={eventData.names_list}
-		handleInputChange={handleCalendarChange}
+		handleCalendarChange={handleCalendarChange}
 		/>     
 	</div>
 )};
