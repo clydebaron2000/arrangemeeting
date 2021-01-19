fee036002c7292936149a4a920583a2f1c62705b
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2021-01-18 17:23:51 -0800

    added existing functionality to calendar.

--- client/src/components/CalenderTable/CalenderTable.jsx
+++ client/src/components/CalenderTable/CalenderTable.jsx
@@ -1,80 +1,79 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 import './styles.css';
 import DayColumn from '../DayColumn/DayColumn'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faChevronLeft,faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import AvailabilityContext from '../../contexts/availabilityContext.jsx';
 function Calendar(props){          
-  const [validDates]=useState(props.valid_dates)    
-  const [validTimes]=useState(props.valid_times)   
-  const [calendarMatrix,setCalendarMatrix]=useState(props.calendar_matrix)    
-  const [isSelecting,setIsSelecting]=useState(false)//initialize to false  
-  const [renderedColumns,setRenderedColumns]=useState((<div></div>))
- 
-  const handleDayChange=(dayChangeObj)=>{
-    const dayIndex=dayChangeObj.dayIndex
-    const dayData=dayChangeObj.dayData
-    let tempMatrix=calendarMatrix
-    tempMatrix[dayIndex]=dayData
-    setCalendarMatrix(tempMatrix)
-  }
-  const mouseUp=_=>{if(props.isEditing===true)setIsSelecting(false)}
-  const mouseDown=_=>{if(props.isEditing===true)setIsSelecting(true)}
+  const context =useContext(AvailabilityContext)
+  const [visIndexes,setVisIndexes]=useState({
+    start:0,
+    end:(context.validDates.length<7)?context.validDates.length:7
+  })
   
-  //new
-  useEffect(_=>{
-    props.handleAvailabilityChange(calendarMatrix)
-  },[calendarMatrix])
-
-
-  //
+  
+  const leftArrow=_=>{
+    console.log("l")
+    let min =0
+    let inc=Math.max(-7-1,-visIndexes.start)
+    let out=visIndexes
+    if (inc+out.start<min) return;
+    out.end=out.start
+    out.start+=inc
+    setVisIndexes(out)
+  }
+  const rightArrow=_=>{
+    console.log("r")
+    let max=context.validDates.length;
+    let inc=Math.min(7,max-visIndexes.end)
+    let out=visIndexes
+    if (inc+out.end>max) return;
+    out.start=out.end
+    out.end+=inc
+    setVisIndexes(out)
+  }
   useEffect(()=>{
-    console.log('caltable change',isSelecting)
-    console.log(calendarMatrix)
-    setRenderedColumns(
-      (calendarMatrix!==undefined)?[...Array(calendarMatrix.length).keys()].map(i=>{
-        return (<DayColumn
-          date={validDates[i]}
-          hourArray={calendarMatrix[i]}
-          classname="dayColumn"
-          isSelecting={isSelecting}
-          filtered_name={props.filtered_name}
-          handleDayChange={handleDayChange}
-          dayIndex={i}
-          key={i}
-        /> ) }
-        ):null
-    )
-  },[isSelecting])
+    console.log('eff',visIndexes)
+  },[context,visIndexes.start,visIndexes.end])
   return (
     <div className="calendarTable" >
-      <button className="l">
+      {/* <button className={(visIndexes.start<=0)?"hide":"l"} onClick={leftArrow}>
         <FontAwesomeIcon icon={faChevronLeft}/>
-      </button>
+      </button> */}
       <div id="timeLabelColumn">
-        {(validTimes.start!==undefined)?(
+        {(context.validTimes.start!==undefined)?(
           <div className="Timezone" defaultValue=''>
-            {validTimes.start.toString().split(' ')[5].split('-').map(t=>
-              <p>{t}</p>
+            {context.validTimes.start.toString().split(' ')[5].split('-').map(t=>
+              <p key={t}>{t}</p>
               )}
           </div> 
         ):null}
-        {(validTimes.start!==undefined)?(
-          [...Array(parseInt(Math.ceil((validTimes.end.valueOf()-validTimes.start.valueOf())/1000/60/60))).keys()].map(i=>
+        {(context.validTimes.start!==undefined)?(
+          [...Array(parseInt(Math.ceil((context.validTimes.end.valueOf()-context.validTimes.start.valueOf())/1000/60/60))).keys()].map(i=>
           <div className="time" key={i}>{
-            new Date(validTimes.start.valueOf()+i*60*60*1000).toLocaleTimeString().replace(":00:00",'')
+            new Date(context.validTimes.start.valueOf()+i*60*60*1000).toLocaleTimeString().replace(":00:00",'')
           }</div>
           ))
         :null}   
       </div>
       
-      <div id="ac_grid" onMouseUp={mouseUp} onMouseDown={mouseDown}>
-        {
-        renderedColumns
+      <div id="ac_grid">
+        {(context.tempCalMatrix!==undefined)?[...Array(visIndexes.end-visIndexes.start).keys()].map(i=>{
+          return (<DayColumn
+            // date={context.validDates[visIndexes.start+i]}
+            // hourArray={context.tempCalMatrix[visIndexes.start+i]}
+            // classname="dayColumn"
+            // filtered_name={props.filtered_name}
+            // handleDayChange={()=>{}}
+            dayIndex={visIndexes.start+i}
+            key={visIndexes.start+i}
+          /> ) }
+          ):null
         }
       </div>
-        <button className="r">
+        {/* <button className={(visIndexes.end>=context.validDates.length)?"hide":"r"} onClick={rightArrow}>
         <FontAwesomeIcon icon={faChevronRight}/>
-        </button>
+        </button> */}
     </div>
   )
 }
